3aded6b88c60cffacd951a1f97d1b66936c77199
Jeffrey <135301772+CD4Helper@users.noreply.github.com>
2025-09-23 11:45:15 -0700

    Update Currency.java

--- Currency.java
+++ Currency.java
@@ -1,34 +1,61 @@
-import java.util.Scanner; 
-
-/* Description: this program converts currency
- * Author: Jeff Chen 
- * Author: Arturo S. 
- * Since: 9/17/2025
+/** 
+ * Description: this program converts currency
+ * @author Jeffrey Chen 
+ * @author Arturo S. 
+ * @since 9/17/2025
  */
 
-public class Currency {
-    static final double DOLLARS_TWD_RATIO = .0033 ; 
+public class Currency { 
     public static void welcome() { 
         System.out.println("+++++++++++++++++++++++++++++++++++++++"); 
-        System.out.println("This program converts currency from TWD to USD."); 
-        System.out.println("Or this program will convert from USD to TWD."); 
+        System.out.println("This program converts following currency to USD in both way:"); 
+        System.out.println("Taiwan Dollar and Japanese Yen."); 
         System.out.println("+++++++++++++++++++++++++++++++++++++++");  
     }
 
+public class Conversion {
+     
+    static final double DOLLAR_YEN_RATIO = 147.71;
+    static final double DOLLARS_TWD_RATIO = 30.29 ;
 
-        /** 
-         * Main method 
-         * @param args command line arguments
-         */
+    public static double twd_to_dollar(double yen) {
+        return yen / DOLLAR_YEN_RATIO;
+    }
 
-         public static void main(String[] args) {
+    public static double dollar_to_twd(double dollar) {
+        return dollar * DOLLAR_YEN_RATIO;
+    }
+    
+    /**
+    * This method receives the option input by user and choose type of conversion
+    * @param int 1-4
+    * @return input conversion method
+    */
+    
+    public static int menu() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What type of conversion you want?");
+        System.out.println("1. Yen to Dollar");
+        System.out.println("2. Dollar to Yen");
+        System.out.println("3. Taiwan Dollar to Dollar");
+        System.out.println("4. Dollar to Taiwan Dollar");
+        System.out.println("Please enter number 1-4: ");
+        return input.nextInt();
+    }
+
+       /** 
+       * Main method 
+       * @param args command line arguments
+       */
+
+        public static void main(String[] args) {
           
         welcome(); //function call 
         System.out.println("Enter the type of currency to convert.");
         System.out.println("If USD, enter: 1 ");
         System.out.println("If TWD, enter: 2 ");  
         Scanner in = new Scanner(System.in); //instatiating a scanner object 
-        int choice = in.nextInt();
+        int choice = menu();
         int value; 
 
         switch (choice ) { 
@@ -57,3 +84,4 @@ public class Currency {
          }
 
 }
+
